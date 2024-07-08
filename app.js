4b0718b9a3ed071e95e55b881c5b18e27065f084
Александр <saneksrt222@gmail.com>
2024-07-08 18:56:14 +0400

    full

--- app.js
+++ app.js
@@ -1,24 +1,65 @@
-const createInput = document.getElementById('create__input');
+const inputElement = document.getElementById('create__input');
 const createButton = document.getElementById('create__button');
-const list = document.getElementById('nodes');
+const listElement = document.getElementById('nodes');
 
-function getNote(input){
+const notes = [
+	{
+		title: 'дописать тудушку',
+		completed: false,
+	},
+	{
+		title: 'смотреть реакт',
+		completed: false,
+	},
+];
+
+function render() {
+	listElement.innerHTML = '';
+	for(let i = 0; i < notes.length; i++) {
+		listElement.insertAdjacentHTML('beforeend', getNote(notes[i], i));
+	}
+}
+
+render();
+
+createButton.onclick = function() {
+	if (inputElement.value.length === 0) {
+		return
+	}
+
+	const newNote = {
+		title: inputElement.value,
+		completed: false,
+	}
+
+	notes.push(newNote);
+	render();
+	inputElement.value = '';
+}
+
+listElement.onclick = function(event) {
+	if (event.target.dataset.index) {
+		const index = Number(event.target.dataset.index);
+		const type = event.target.dataset.type;
+
+		if (type === 'toggle'){
+			notes[index].completed = !notes[index].completed;
+		} else if (type === 'remove'){
+			notes.splice(index, 1);
+		}
+
+		render();
+	}
+}
+
+function getNote(note, index){
 	return `
 			<li class="app__item">
-				<span class="app__item-title">${input.value}</span>
+				<span class="app__item-title ${note.completed ? 'close' : ''}">${note.title}</span>
 				<div class="app__buttons">
-					<button class="app__item-btn">✔</button>
-					<button class="app__item-btn">✖</button>
+					<button class="app__item-btn" data-index="${index}" data-type="toggle">✔</button>
+					<button class="app__item-btn" data-index="${index}" data-type="remove">✖</button>
 				</div>
 			</li>
 		`
 }
-
-createButton.onclick = function() {
-	if (createInput.value.length === 0) {
-		return
-	}
-
-	list.insertAdjacentHTML('beforeend', getNote(createInput));
-}
-
